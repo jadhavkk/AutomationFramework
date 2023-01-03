ebd402927aadd7e253b4d7c3932450eeabd27eed
komalj <jadhavkk39@gmail.com>
2023-01-03 16:52:59 +0530

    Creation of Automationframework 3 Jan

--- EmployeeManagement/LoginTest1.cs
+++ EmployeeManagement/LoginTest1.cs
@@ -1,20 +1,38 @@
+using NUnit.Framework.Internal;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace EmployeeManagement
 {
-    public class Tests
+    public class LoginUITests
     {
+        IWebDriver driver;
 
-        [Test]
-        public void ValidateTitleTest()
-        {
-            IWebDriver driver = new ChromeDriver();
+        [SetUp]
+        public void BeforeMethod() {
 
+            driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
             driver.Url = "https://opensource-demo.orangehrmlive.com/";
 
+
+        }
+
+        [TearDown]
+        public void AfterMethod()
+        {
+
+            driver.Quit();
+        }
+        
+        [Test]
+        public void ValidateTitleTest() { 
+        
+
+        
+
+            //driver.Url = "https://google";
 
             string actualTitle = driver.Title;
             Console.WriteLine(actualTitle);
@@ -23,11 +41,30 @@ namespace EmployeeManagement
 
         }
 
+          [Test]
+          public void ValidatePlaceholderTest()
+        {
+
+           
+            //driver.FindElement(By.Name("Username")).Clear();
+
+            string actualUsernamePlaceholder=driver.FindElement(By.Name("username")).GetAttribute("placeholder");
+            Assert.That(actualUsernamePlaceholder, Is.EqualTo("Username"));
+
+            string actualPasswordPlaceholder = driver.FindElement(By.Name("password")).GetAttribute("placeholder");
+            Assert.That(actualPasswordPlaceholder, Is.EqualTo("Password"));
+
+
+
+
+        }
+
+
 
 
    
 
-            //driver.Url = "https://google";
+         
 
            
             
